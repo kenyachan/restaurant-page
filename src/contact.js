2643a94613f88e0f267a2c0a56d5646a9099882f
Kenya Chan <kenya.chan@gmail.com>
2022-11-14 13:00:08 +1100

    Update location to object

--- src/contact.js
+++ src/contact.js
@@ -11,32 +11,84 @@ export default function contactPage() {
     pageHeading.textContent = 'Contact Us';
     pageBlock.appendChild(pageHeading);
 
-    pageBlock.appendChild(location());
+    const location = Location('123', 'Fake St', 'Suburb', 'STATE', '2000');
+    const locationElement = location.getLocationElement();
+
+    pageBlock.appendChild(locationElement);
     pageBlock.appendChild(operatingHours());
     pageBlock.appendChild(phoneNumber());
 
     return contactPage;
 }
 
-function location() {
-    const location = document.createElement('section');
-    location.setAttribute('id', 'address');
-    location.classList.add('info-block');
+const Location = (number, street, suburb, state, postCode) => {
+    const _addressLine1 = `${number} ${street}`;
+    const _addressLine2 = `${suburb}, ${state} ${postCode}`;
+    const locationElement = createLocationElement(_addressLine1, _addressLine2);
 
-    const heading = document.createElement('h2');
-    heading.textContent = 'Location';
-    location.appendChild(heading);
+    function createLocationElement(addressLine1, addressLine2) {
+        const locationElement = document.createElement('section');
+        locationElement.setAttribute('id', 'address');
+        locationElement.classList.add('info-block');
 
-    const addressLine1 = document.createElement('p');
-    addressLine1.textContent = '123 Totally Real Address';
-    location.appendChild(addressLine1);
-    
-    const addressLine2 = document.createElement('p');
-    addressLine2.textContent = 'Good Suburb, STATE 2000';
-    location.appendChild(addressLine2);
+        const heading = document.createElement('h2');
+        heading.textContent = 'Location';
+        locationElement.appendChild(heading);
 
-    return location;
+        const addressLine1Element = document.createElement('p');
+        addressLine1Element.textContent = addressLine1;
+        locationElement.appendChild(addressLine1Element);
+        
+        const addressLine2Element = document.createElement('p');
+        addressLine2Element.textContent = addressLine2;
+        locationElement.appendChild(addressLine2Element);
+
+        return locationElement;
+    }
+
+    const getAddress = () => {
+        return `${_addressLine1}, ${_addressLine2}`;
+    }
+
+    const getAddressLine1 = () => {
+        return _addressLine1;
+    }
+
+    const getAddressLine2 = () => {
+        return _addressLine2;
+    }
+
+    const getLocationElement = () => {
+        return locationElement;
+    }
+
+    return {
+        getLocationElement,
+        getAddress,
+        getAddressLine1,
+        getAddressLine2,
+    }
 }
+
+// function location() {
+//     const location = document.createElement('section');
+//     location.setAttribute('id', 'address');
+//     location.classList.add('info-block');
+
+//     const heading = document.createElement('h2');
+//     heading.textContent = 'Location';
+//     location.appendChild(heading);
+
+//     const addressLine1 = document.createElement('p');
+//     addressLine1.textContent = '123 Totally Real Address';
+//     location.appendChild(addressLine1);
+    
+//     const addressLine2 = document.createElement('p');
+//     addressLine2.textContent = 'Good Suburb, STATE 2000';
+//     location.appendChild(addressLine2);
+
+//     return location;
+// }
 
 function operatingHours() {
     const operatingHours = document.createElement('section');
